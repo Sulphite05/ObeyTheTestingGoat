48755dc62614e3dcef5e6bce7cafa150aa7d2b22
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-08-25 22:18:09 +0500

    First FT specced out in comments and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,12 +1,4 @@
-from selenium import webdriver
-
-browser = webdriver.Firefox()
-
-browser.get("http://localhost:8000")
-
-assert "Congratulations" in browser.title   # should type congratulations to map
-
-print("Okay!")  # failing test if superlists are not configured
+# failing test if superlists are not configured
 # django-admin startproject superlists .
 # The superlists folder is intended for stuff that applies to the whole
 # project- like settings.py, for example, which is used to store global
@@ -16,5 +8,50 @@ print("Okay!")  # failing test if superlists are not configured
 # python manage.py runserver
 # test passed after this
 
+# Tests that use Selenium let us drive a real web browser, so they really
+# let us see how the application functions from the user’s point of view.
+# That’s why they’re called functional tests.
+# Functional Test == Acceptance Test == End-to-End Test
+
+import unittest
+from selenium import webdriver
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+
+        # Aqiba has heard about a new To Do list App
+        # She checks its homepage
+        self.browser.get("http://localhost:8000")
+
+        # She notices the pagge title and header mention to do lists
+        self.assertIn("To-Do", self.browser.title)
+
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish the test!")
+        [...]
+        # She types "Knead Dough" in  a text-box
+
+        # When she hits enter, the page updates and shows "1. Knead Dough" on the page
+
+        # There is still a text-box inviting her to add a new item to the list
+
+        # She enters "Roll the rotis" as she is very methodical and presses enter
+
+        # The page updates again and shows both items on the page
+
+        # Satisfied, she goes back to sleep
 
 
+if __name__ == "__main__":
+    # that’s how a Python script checks if it’s been executed from the command line,
+    # rather than just imported by another script
+
+    unittest.main()     # searches for all tests in main files then runs them
+
+# git commit -a to automatically add any changes to tracked files(not any newly added file)
