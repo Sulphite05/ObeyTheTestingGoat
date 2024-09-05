5d250bd372aeeca8e46dd2abbb3e65e79707f4e4
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-09-05 20:18:56 +0500

    Added List object and foreign key relation with Item

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
-# from django.http import HttpRequest
-# from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
+
 
 
 # Create your tests here.
@@ -58,15 +57,23 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-class ItemModelTest(TestCase):
-    def test_saving_and_retrieving_item(self):
+class ListAndItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        my_list = List()
+        my_list.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = my_list
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = my_list
         second_item.save()
+
+        saved_list = List.objects.get()
+        self.assertEqual(saved_list, my_list)
 
         saved_items = Item.objects.all()    # returns QuerySet which is a list-like object
         self.assertEqual(saved_items.count(), 2)
@@ -75,7 +82,9 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, my_list)
         self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, my_list)
 
 
 class ListViewTest(TestCase):
@@ -84,8 +93,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, "list.html")
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        my_list = List.objects.create()
+        Item.objects.create(text="itemey 1", list=my_list)
+        Item.objects.create(text="itemey 2", list=my_list)
         response = self.client.get("/lists/the-only-list-in-the-world/")
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
