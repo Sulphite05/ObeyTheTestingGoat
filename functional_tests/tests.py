8aafdcca4149fcd72205d02ae94770bb4e2f89f0
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-09-07 20:04:01 +0500

    First steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -31,6 +31,38 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+
+    def test_layout_and_styling(self):
+
+        # Aqiba goes to the homepage
+        self.browser.get(self.live_server_url)
+
+        # Her browser window is set to a very specific size
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box isnicely centered
+        inputbox =self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta=10,
+        )
+
+        # She starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys("testing")
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_todo_list("1. testing")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] /2,
+            512,
+            delta=10,
+        )
+
+
+
+
     def wait_for_row_in_todo_list(self, row_text):
         start_time = time.time()
         while True:
