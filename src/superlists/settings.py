9c219a803b581bd60c6d0f7f5d754ec5b7b7cda1
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2024-09-13 20:20:16 +0500

    Add collectstatic to dockerfile, nd new location to .gitignore

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,16 @@ SECRET_KEY = 'django-insecure-zgh%g@3-+m^y$a2=gl9vvd+133go^sb0*+ealmsnu(=smz9goi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+if "DJANGO_DEBUG_FALSE" in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+    ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
+else:
+    DEBUG = True
+    SECRET_KEY = "insecure-key-for-dev"
+    ALLOWED_HOSTS = []
+
+
 
 
 # Application definition
